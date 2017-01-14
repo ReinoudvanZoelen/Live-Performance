878a74b07166d754daf0b2826c8a8d12b3c644d5
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-14 23:52:24 +0100

    Added the database class and some required folders

---
+++ Live-Performance/Repositories/MSSQLRepository/Database.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Live_Performance.Repositories.MSSQLRepository
+{
+    public class Database
+    {
+        private string Server;
+        private string DB;
+        private string Uid;
+        private string Password;
+        public SqlConnection Connection;
+        
+        public bool OpenConnection()
+        {
+            Server = "mssql.fhict.local";
+            DB = "dbi354261";
+            Uid = "dbi354261";
+            Password = "EYECT4RAILS";
+            string connectionString = "SERVER=" + Server + ";" + "DATABASE=" + 
+                DB + ";" + "UID=" + Uid + ";" + "PASSWORD=" + Password + ";";
+
+            Connection = new SqlConnection(connectionString); 
+
+            try
+            {
+                Connection.Open();
+                return true;
+            }
+            catch (SqlException)
+            {
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                Connection.Close();
+                return true;
+            }
+            catch (SqlException exception)
+            {
+                MessageBox.Show(exception.ToString());
+                return false;
+            }
+        }
+
+    }
+}
