752a11967af2a75913139d1c544d1e2c02646004
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-15 09:54:54 +0100

    Added the UnitTest project which now holds a template

---
+++ Live-Performance-UnitTest/TU_MSSQLTemplateRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Live_Performance.Repositories.MSSQLRepository;
+
+namespace Live_Performance_UnitTest
+{
+    [TestClass]
+    public class TU_MSSQLTemplateRepository
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            // Arrange
+            MSSQLTemplateRepository rep = new MSSQLTemplateRepository();
+            // Act
+
+            // Assert
+
+        }
+
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+
+        [TestMethod]
+        public void Insert()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+
+        [TestMethod]
+        public void Update()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
