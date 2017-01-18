b548be4c26312f3b1868f8432a0ace98bee0c670
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-19 00:04:47 +0100

    Added some better constructors

--- Live-Performance/Classes/Ingredient.cs
+++ Live-Performance/Classes/Ingredient.cs
@@ -8,7 +8,22 @@ namespace Live_Performance.Classes
 {
     public class Ingredient:Product
     {
+
         public bool Halal { get; set; }
         public bool Veganistisch { get; set; }
+
+        public Ingredient()
+        {
+        }
+
+        public Ingredient(int id, string naam, decimal verkoopprijs, decimal inkoopprijs, bool halal, bool veganistisch)
+        {
+            this.ID = id;
+            this.Naam = naam;
+            this.Verkoopprijs = verkoopprijs;
+            this.Inkoopprijs = inkoopprijs;
+            this.Halal = halal;
+            this.Veganistisch = veganistisch;
+        }
     }
 }
--- Live-Performance/Classes/Pizza.cs
+++ Live-Performance/Classes/Pizza.cs
@@ -8,11 +8,39 @@ namespace Live_Performance.Classes
 {
     public class Pizza : Product
     {
-        public double Oppervlakte { get; set; }
         public string Bodemtype { get; set; }
-        public string Vorm { get; set; }
+        public double Oppervlakte { get; set; }
+        public int[] Afmetingen { get; set; } 
         public List<Ingredient> Ingredienten { get; set; }
         public bool Custom { get; set; }
         public bool Glutenvrij { get; set; }
+
+        public Pizza()
+        {
+        }
+
+        public Pizza(string naam, bool custom, bool glutenvrij, int[] afmetingen, List<Ingredient> ingredienten)
+        {
+            this.Naam = naam;
+            this.Custom = custom;
+            this.Afmetingen = afmetingen;
+            this.Glutenvrij = glutenvrij;
+            this.Ingredienten = ingredienten;
+        }
+
+        public Pizza(int id, string naam, decimal verkoopprijs, decimal inkoopprijs, string bodemtype,
+            double oppervlakte, int[] afmetingen, bool custom, bool glutenvrij, List<Ingredient> ingredienten)
+        {
+            this.ID = id;
+            this.Naam = naam;
+            this.Verkoopprijs = verkoopprijs;
+            this.Inkoopprijs = inkoopprijs;
+            this.Bodemtype = bodemtype;
+            this.Oppervlakte = oppervlakte;
+            this.Afmetingen = afmetingen;
+            this.Custom = custom;
+            this.Glutenvrij = glutenvrij;
+            this.Ingredienten = ingredienten;
+        }
     }
 }
