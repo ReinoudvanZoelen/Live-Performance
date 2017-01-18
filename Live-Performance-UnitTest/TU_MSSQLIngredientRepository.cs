d6d0c29ba7c26bdd7f9c9476b2f8147c88496f71
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-19 00:03:50 +0100

    Added unit tests for Drank, Ingredient and Pizza. Made a playlist file for them, they all work 100%.

---
+++ Live-Performance-UnitTest/TU_MSSQLIngredientRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Live_Performance.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Live_Performance.Repositories.MSSQLRepository;
+
+namespace Live_Performance_UnitTest
+{
+    [TestClass]
+    public class TU_MSSQLIngredientRepository
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            // Arrange
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            List<Ingredient> ingredients = IngredientRepository.GetAll();
+
+            // Act
+            int count = ingredients.Count;
+
+            // Assert
+            Assert.AreNotEqual(0, ingredients.Count);
+        }
+
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            Ingredient ingredient = IngredientRepository.GetById(9);
+
+            // Act
+
+
+            // Assert
+            Assert.AreEqual("Paprika", ingredient.Naam);
+        }
+
+
+        [TestMethod]
+        public void Insert()
+        {
+            // Arrange
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            Ingredient ingredient = new Ingredient(-1, "Sugar and Spice", new decimal(5), new decimal(3), false, true);
+
+            // Act
+            int startCount = IngredientRepository.GetAll().Count;
+
+            IngredientRepository.Insert(ingredient);
+
+            int endCount = IngredientRepository.GetAll().Count;
+
+            // Assert
+            Assert.AreEqual(startCount + 1, endCount);
+        }
+
+
+        [TestMethod]
+        public void Update()
+        {
+            // Arrange
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            List<Ingredient> ingredienten = IngredientRepository.GetAll();
+            Ingredient ingredient = ingredienten[ingredienten.Count - 1];
+            Ingredient verifyIngredient;
+
+            // Act
+            ingredient.Naam = "And everything nice!";
+            ingredient.Halal = true;
+            ingredient.Veganistisch = false;
+
+            IngredientRepository.Update(ingredient);
+
+            ingredienten = IngredientRepository.GetAll();
+            verifyIngredient = ingredienten[ingredienten.Count - 1];
+
+            // Assert
+            Assert.AreEqual("And everything nice!", verifyIngredient.Naam);
+            Assert.AreEqual(true, verifyIngredient.Halal);
+            Assert.AreEqual(false, verifyIngredient.Veganistisch);
+        }
+
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            List<Ingredient> ingredienten = IngredientRepository.GetAll();
+            Ingredient ingredient = ingredienten[ingredienten.Count - 1];
+
+            // Act
+            int startCount = IngredientRepository.GetAll().Count;
+
+            IngredientRepository.Delete(ingredient.ID);
+
+            int endCount = IngredientRepository.GetAll().Count;
+
+            // Assert
+            Assert.AreEqual(startCount - 1, endCount);
+        }
+    }
+}
---
+++ Live-Performance-UnitTest/TU_MSSQLPizzaRepository.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using Live_Performance.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Live_Performance.Repositories.MSSQLRepository;
+
+namespace Live_Performance_UnitTest
+{
+    [TestClass]
+    public class TU_MSSQLPizzaRepository
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            // Arrange
+            MSSQLPizzaRepository PizzaRepository = new MSSQLPizzaRepository();
+            List<Pizza> pizzas = PizzaRepository.GetAll();
+
+            // Act
+            int count = pizzas.Count;
+
+            // Assert
+            Assert.AreNotEqual(0, count);
+        }
+
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            MSSQLPizzaRepository PizzaRepository = new MSSQLPizzaRepository();
+            Pizza pizza = PizzaRepository.GetById(4);
+
+            // Act
+            string naam = pizza.Naam;
+
+            // Assert
+            Assert.AreEqual("Margherita", naam);
+        }
+
+
+        [TestMethod]
+        public void Insert()
+        {
+            // Arrange
+            MSSQLPizzaRepository PizzaRepository = new MSSQLPizzaRepository();
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+
+
+            // Act
+            int[] afmetingen = new int[3];
+            afmetingen[0] = 20;
+            afmetingen[1] = 25;
+
+
+            // Echt een hoop shit en een dikke bodem
+            List<Ingredient> ingredienten = new List<Ingredient>();
+            ingredienten.Add(IngredientRepository.GetById(1));
+            ingredienten.Add(IngredientRepository.GetById(3));
+            ingredienten.Add(IngredientRepository.GetById(6));
+            ingredienten.Add(IngredientRepository.GetById(11));
+            ingredienten.Add(IngredientRepository.GetById(14));
+
+            Pizza pizza = new Pizza("Pizza Magnifico!", true, true, afmetingen, ingredienten);
+
+            PizzaRepository.Insert(pizza);
+
+            List<Pizza> pizzas = PizzaRepository.GetAll();
+            Pizza verifyPizza = pizzas[pizzas.Count - 1];
+
+            // Assert
+            Assert.AreEqual(pizza.Naam, verifyPizza.Naam);
+        }
+
+
+        [TestMethod]
+        public void Update()
+        {
+            // Arrange
+            MSSQLPizzaRepository PizzaRepository = new MSSQLPizzaRepository();
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            List<Pizza> Pizzas = PizzaRepository.GetAll();
+            Pizza pizza = Pizzas[Pizzas.Count - 1];
+            Pizza verifyPizza;
+
+            // Act
+
+            pizza.Naam = "Vieze glutenvrije tomaten/olijvenpizza";
+
+            pizza.Ingredienten.Clear();
+            pizza.Ingredienten.Add(IngredientRepository.GetById(2));
+            pizza.Ingredienten.Add(IngredientRepository.GetById(7));
+            pizza.Ingredienten.Add(IngredientRepository.GetById(15));
+
+            int[] afmetingen = new int[3];
+            afmetingen[0] = 20;
+            afmetingen[1] = 25;
+            afmetingen[2] = 30;
+
+            pizza.Afmetingen = afmetingen;
+
+            pizza.Glutenvrij = false;
+            pizza.Custom = false;
+
+            PizzaRepository.Update(pizza);
+
+            Pizzas = PizzaRepository.GetAll();
+            verifyPizza = Pizzas[Pizzas.Count - 1];
+
+            // Assert
+            Assert.AreNotEqual(4, verifyPizza.Ingredienten.Count);
+        }
+
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            MSSQLPizzaRepository PizzaRepository = new MSSQLPizzaRepository();
+            MSSQLIngredientRepository IngredientRepository = new MSSQLIngredientRepository();
+            List<Pizza> Pizzas = PizzaRepository.GetAll();
+            Pizza pizza = Pizzas[Pizzas.Count - 1];
+
+            // Act
+            int startCount = PizzaRepository.GetAll().Count;
+
+            PizzaRepository.Delete(pizza.ID);
+
+            int endCount = PizzaRepository.GetAll().Count;
+
+            // Assert
+            Assert.AreEqual(startCount - 1, endCount);
+        }
+    }
+}
+
