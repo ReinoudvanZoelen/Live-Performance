cf92fd4607c421652a253cce15f5a34bd559b1cd
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-19 01:26:39 +0100

    Added logic for the first page (relatively empty)

---
+++ Live-Performance/Logic/MainFormLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Live_Performance.Repositories.Context;
+using Live_Performance.Repositories.MSSQLRepository;
+
+namespace Live_Performance.Logic
+{
+    public class MainFormLogic
+    {
+        public KlantContext KlantContext = new KlantContext(new MSSQLKlantRepository());
+        public PizzaContext PizzaContext = new PizzaContext(new MSSQLPizzaRepository());
+        public DrankContext DrankContext = new DrankContext(new MSSQLDrankRepository());
+        public SaladeContext SaladeContext = new SaladeContext(new MSSQLSaladeRepository());
+        public IngredientContext IngredientContext = new IngredientContext(new MSSQLIngredientRepository());
+    }
+}
