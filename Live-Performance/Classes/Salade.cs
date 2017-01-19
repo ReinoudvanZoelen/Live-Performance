15d9b7e4788d80e1121a8f2e54e4786fda229853
Reinoud van Zoelen <354261@student.fontys.nl>
2017-01-19 01:25:50 +0100

    Added a constructor to Salade

--- Live-Performance/Classes/Salade.cs
+++ Live-Performance/Classes/Salade.cs
@@ -8,5 +8,17 @@ namespace Live_Performance.Classes
 {
     public class Salade:Product
     {
+        public Salade(int id, string naam, decimal verkoopprijs, decimal inkoopprijs)
+        {
+            this.ID = id;
+            this.Naam = naam;
+            this.Verkoopprijs = verkoopprijs;
+            this.Inkoopprijs = inkoopprijs;
+        }
+
+        public Salade()
+        {
+            
+        }
     }
 }
